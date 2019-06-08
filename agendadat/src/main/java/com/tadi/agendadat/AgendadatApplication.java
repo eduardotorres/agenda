7045de6ead16e759879205b4382d2e863078be98
eduardotorres <dudutorres00@outlook.com>
2019-06-08 20:39:30 -0300

    .

---
+++ agendadat/src/main/java/com/tadi/agendadat/AgendadatApplication.java
@@ -0,0 +1,13 @@
+package com.tadi.agendadat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadatApplication.class, args);
+	}
+
+}
---
+++ agendadat/src/main/java/com/tadi/agendadat/repository/ContatoRepository.java
@@ -0,0 +1,9 @@
+package com.tadi.agendadat.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.tadi.agendadat.model.Contato;
+
+public interface ContatoRepository  extends JpaRepository<Contato, Long>{
+
+}
